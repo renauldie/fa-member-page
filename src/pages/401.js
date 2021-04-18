432efefff7fe03c077da3bbe9eeeb48a3be151e7
renauldie <renauldizanagi@gmail.com>
2021-04-18 10:38:11 +0700

    make error page. when user did something wrong

--- src/pages/401.js
+++ src/pages/401.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-export default function NotFound() {
-  return (
-    <div>
-      Unauthtenticated
-    </div>
-  )
+import { Link } from 'react-router-dom';
+
+export default function Unauthenticated({
+	fallbackUrl,
+	fallbackText,
+	external,
+}) {
+	return (
+		<section className='h-screen flex flex-col items-center mt-10'>
+			<img
+				style={{ height: 400, zIndex:100}}
+				src={`${process.env.PUBLIC_URL}/assets/images/illustration-not-found.jpg`}
+				alt='you are not supposed here, please login'
+			/>
+			<h1 className='text-2xl text-gray-900 mt-12'>No Atuhorization Found.</h1>
+			<p className='text-md text-gray-600 mt-4 mtb-8 mx-auto text-center'>
+				this page is publically available. <br/>
+        to access it please login first.
+			</p>
+			{external ? (
+				<a
+					className='bg-orange-500 hover:bg-orange-400 transition-all duration-200 focus:outline-none shadow-inner text-white px-6 py-3 mt-5'
+					href={fallbackUrl}>
+					{fallbackText || 'Logging me in'}
+				</a>
+			) : (
+				<Link
+					className='bg-blue-500 hover:bg-blue-600 transition-all duration-200 focus:outline-none shadow-inner text-white px-6 py-3 mt-5'
+					to={fallbackUrl || '/login'}>
+					{fallbackText || 'Logging me in'}
+				</Link>
+			)}
+		</section>
+	);
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-export default function Unauthtenticate() {
-  return (
-    <div>
-      Login
-    </div>
-  )
+import { Link } from 'react-router-dom';
+
+export default function NotFound() {
+	return (
+		<section className='h-screen flex flex-col items-center mt-10'>
+			<img
+				style={{ height: 400, zIndex: 100 }}
+				src={`${process.env.PUBLIC_URL}/assets/images/illustration-not-found.jpg`}
+				alt='you are not supposed here, please login'
+			/>
+			<h1 className='text-2xl text-gray-900 mt-12'>This Page Not Found</h1>
+			<p className='text-md text-gray-600 mt-4 mtb-8 mx-auto text-center'>
+				Maybe you went to wrong place. <br />
+			</p>
+			<Link
+				className='bg-blue-500 hover:bg-blue-600 transition-all duration-200 focus:outline-none shadow-inner text-white px-6 py-3 mt-5'
+				to={'/'}>
+				Back to home
+			</Link>
+		</section>
+	);
 }
