2b667b143cd70341c5e7b2b8bdf3b7d24ce8d728
renauldie <renauldizanagi@gmail.com>
2021-04-18 10:42:25 +0700

    token handler token

--- src/configs/axios/errorHandler.js
+++ src/configs/axios/errorHandler.js
@@ -1,16 +1,49 @@
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify';
+import users from 'constants/api/users';
+
+import axios, { setAuthorizationHeader } from './index';
 
 export default function errorHandler(error) {
-  if (error) {
-    let message;
-    if (error.response) {
-      if (error.response.status === 500)
-        message = "Something went terribly wrong";
-      else message = error.response.data.message;
+	if (error) {
+		let message;
+		if (error.response) {
+			const originalRequest = error.config;
+			if (error.response.status === 500)
+				message = 'Something went terribly wrong';
+			else if (error.response.status === 403 && !originalRequest._retry) {
+				originalRequest._retry = true;
+				const session = localStorage['FA:token']
+					? JSON.parse(localStorage['FA:token'])
+					: null;
+				return users
+					.refresh({
+						refresh_token: session.refresh_token,
+						email: session.email,
+					})
+					.then((res) => {
+						if (res.data) {
+							setAuthorizationHeader(res.data.token);
+							localStorage.setItem(
+								'FA:token',
+								JSON.stringify({
+									...session,
+									token: res.data.token,
+								})
+							);
 
-      if (typeof message === "string") toast.error(message);
+							originalRequest.headers.authorization = res.data.token;
 
-      return Promise.reject(error);
-    }
-  }
+							return axios(originalRequest);
+						} else {
+							window.location.href = '/login';
+							localStorage.removeItem('FA:token');
+						}
+					});
+			} else message = error.response.data.message;
+
+			if (typeof message === 'string') toast.error(message);
+
+			return Promise.reject(error);
+		}
+	}
 }
