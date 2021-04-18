fabe01c6ddd2276d10b055b2449ffc0ff997099a
renauldie <renauldizanagi@gmail.com>
2021-04-18 10:44:09 +0700

    fixing typo and other variables

--- src/parts/LoginForm.js
+++ src/parts/LoginForm.js
@@ -4,15 +4,16 @@ import { useDispatch } from 'react-redux';
 
 import users from 'constants/api/users';
 
-import { setAuthirizationHeader } from 'configs/axios';
+import { setAuthorizationHeader } from 'configs/axios';
 
 import { populateProfile } from 'store/actions/users';
 
-import useForm from 'helpers/hooks/useform';
+import useForm from 'helpers/hooks/useForm';
 
 function LoginForm({ history }) {
-	const dispacth = useDispatch();
-	const [{u, p}, setState] = useForm({
+	const dispatch = useDispatch();
+
+	const [{ u, p }, setState] = useForm({
 		u: '',
 		p: '',
 	});
@@ -23,12 +24,11 @@ function LoginForm({ history }) {
 		users
 			.login({ u, p })
 			.then((res) => {
-				setAuthirizationHeader(res.data.token);
-				// console.log(res);
+				setAuthorizationHeader(res.data.token);
 				users.details().then((detail) => {
-					dispacth(populateProfile(detail.data));
+					dispatch(populateProfile(detail.data));
 					const production =
-						process.env.REACT_APP_FRONT_PAGE_URL === 'http://localhost:3005'
+						process.env.REACT_APP_FRONTPAGE_URL === 'https://localhost:3005'
 							? 'Domain = localhost:3005'
 							: '';
 					localStorage.setItem(
@@ -60,45 +60,44 @@ function LoginForm({ history }) {
 	}
 
 	return (
-		<div className='container flex justify-center items-center pb-24 -mb-10'>
-			<div className='w-full sm:w-4/12'>
+		<div className='flex justify-center items-center pb-24'>
+			<div className='w-full sm:w-3/12'>
 				<h1 className='text-4xl text-gray-900 mb-6'>
-					<span className='font-bold'>Lets beginn</span> The Journery
+					<span className='font-bold'>Continue</span> Study, <br />
+					Finish your <span className='font-bold'>Goals</span>
 				</h1>
-
 				<form onSubmit={submit}>
-					<div className='flex flex-col mb-4 mt-4'>
-						<label htmlFor='npm' className='text-md mb-2'>
+					<div className='flex flex-col mb-4'>
+						<label htmlFor='u' className='text-lg mb-2'>
 							NPM
 						</label>
 						<input
 							name='u'
-							type='text'
+							type='name'
 							onChange={setState}
-							className='bg-white focus:outline-none border w-full px-6 py-2 border-gray-400 focus:border-blue-600'
+							className='bg-white focus:outline-none border w-full px-6 py-3 w-1/2 border-gray-600 focus:border-teal-500'
 							value={u}
-							placeholder='Your npm'
-							required
+							placeholder='Your email addres'
 						/>
 					</div>
 
 					<div className='flex flex-col mb-4'>
-						<label htmlFor='password' className='text-lg mb-2'>
+						<label htmlFor='p' className='text-lg mb-2'>
 							Password
 						</label>
 						<input
 							name='p'
 							type='password'
 							onChange={setState}
-							className='bg-white focus:outline-none border w-full px-6 py-2 border-gray-400 focus:border-blue-600'
+							className='bg-white focus:outline-none border w-full px-6 py-3 w-1/2 border-gray-600 focus:border-teal-500'
 							value={p}
-							placeholder='Your Password'
-							required
+							placeholder='Your password addres'
 						/>
 					</div>
+
 					<button
 						type='submit'
-						className='bg-indigo-700 hover:bg-indigo-600 transition-all duration-200 focus:outline-none shadow-inner text-white px-6 py-3 mt-4 w-full'>
+						className='bg-blue-700 hover:bg-blue-600 transition-all duration-200 focus:outline-none shadow-inner text-white px-6 py-3 mt-4 w-full'>
 						Masuk
 					</button>
 				</form>
@@ -107,19 +106,20 @@ function LoginForm({ history }) {
 			<div className='w-1/12 hidden sm:block'></div>
 
 			<div className='w-5/12 hidden sm:block justify-end pt-24 pr-16'>
-				<div className='relative' style={{ width: 390, height: 420 }}>
-					<div className='absolute items-center w-full h-full -mb-8 ml-16'>
+				<div className='relative' style={{ width: 369, height: 440 }}>
+					<div className='absolute w-full h-full -mb-8 -ml-8'>
 						<img
-							src='/assets/images/login-character.jpg'
-							alt='Character login'
+							src='#'
+							alt=''
 						/>
-						<div
-							className='absolute z-10 -mt-9 bg-white bottom-0 right-0 py-3 px-4 -mr-10'
-							style={{ width: 290 }}>
-							<p className='text-gray-900 mb-2'>
-								Tidak perlu registrasi, gunakan akun Amikom saja
-							</p>
-						</div>
+					</div>
+					<div
+						className='absolute z-10 bg-white bottom-0 right-0 py-3 px-4 -mr-12'
+						style={{ width: 290 }}>
+						<p className='text-gray-900 mb-2'>
+							Tidak perlu registrasi. Cukup pakai akun mahasiswa
+						</p>
+						<span className='text-gray-600'>~ IT Support Team</span>
 					</div>
 				</div>
 			</div>
