1e437dcf2099271c719aeec4b8eab72628c0bc46
renauldie <renauldizanagi@gmail.com>
2021-04-18 10:39:56 +0700

    add empty state

--- src/pages/MemberArea.js
+++ src/pages/MemberArea.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import Sidebar from 'parts/Sidebar';
+
+function EmptyState() {
+	return (
+		<section className='flex h-screen items-center'>
+			<div className='w-5/12 text-center py-12 mx-auto'></div>
+		</section>
+	);
+}
 
 export default function MemberArea() {
-  return (
-    <div>
-      Member Page
-    </div>
-  )
+	return (
+		<div className='flex'>
+			<Sidebar></Sidebar>
+		</div>
+	);
 }
