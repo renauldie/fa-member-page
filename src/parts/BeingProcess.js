9c1833fcfe3456aa195770f0f3f31397cc2a1b94
renauldie <renauldizanagi@gmail.com>
2021-05-04 22:16:25 +0700

    add being progres parts

---
+++ src/parts/BeingProcess.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BeingProcess() {
+  return (
+    <div>
+      <h1 className='text-lg text-center mt-36 md:ml-36 xl:ml-36'>
+        we are sorry, this page still being development
+      </h1>
+    </div>
+  )
+}
