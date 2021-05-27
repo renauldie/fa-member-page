f34cf33f66dd1a77a1408e6c6b8f3fde936f4acd
renauldie <renauldizanagi@gmail.com>
2021-05-27 07:40:25 +0700

    add delete course api

--- src/constants/api/oprec.js
+++ src/constants/api/oprec.js
@@ -5,4 +5,5 @@ export default {
 	// getData: (code) => axios.get(`/offered-courses/${code}`).then((res) => res.data),
 	joinCourse: (id) => axios.post('/user-course', {id: id}),
 	myCourse: () => axios.get('/user-course'),
+	DelMyCourse: (id) => axios.delete(`/user-course/${id}`),
 };
